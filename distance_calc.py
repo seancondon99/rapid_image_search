f3e25ad13992a2a9cb48f8ccdec61d1fc212076a
Sean Condon <scondon@mit.edu>
2021-09-26 00:12:28 -0400

    clean up distance calc code

--- distance_calc.py
+++ distance_calc.py
@@ -27,43 +27,64 @@ def save_global_dictionary():
             fpath = os.path.join(global_dir, f)
             with open(fpath) as fi:
                 fi_dict = json.load(fi)
+
+        #add elements of local dict to global dict
         for key, value in fi_dict.items():
             global_key = fname + '_' + key
             global_dict[global_key] = value
 
-    savedir = '/Users/seancondon/Desktop/065_final/global.json'
+    savedir = './global.json'
     with open(savedir, 'w+') as fx:
         json.dump(global_dict, fx)
 
 def shorten_img_name(original_string):
-    #  generate UID from image original name by grabbing characters before first underscore
+    '''
+    generate UID from image original name by grabbing characters before first underscore
+
+    :param: original_string: oringinal name to be shortened
+    :return: short_name, string
+    '''
     short_name = original_string.split('_', 1)[0]
     return short_name
 
 
-# transforms dict for img with uid = "test" from {"0" -> [512 floats], ...} to {"test_0 -> [512 floats], ...}
 def concat_uid_with_tile_label(fp_folder_filepath, filename, uid):
-    full_filepath = os.path.join(fp_folder_filepath, filename)
-    # print(full_filepath)
+    '''
+    transforms dict for img with uid = "test" from {"0" -> [512 floats], ...} to {"test_0 -> [512 floats], ...}
 
+    :param fp_folder_filepath: the folder that we are examining files from
+    :param filename: the specific filepath being concatenated
+    :param uid: specifies how to change filename
+    :return: None
+    '''
+
+    #create full filepath from fp_folder_filepath and file
+    full_filepath = os.path.join(fp_folder_filepath, filename)
+
+    #load json dict at full filepath
     with open(full_filepath) as f:
         data = json.load(f)
 
+    #concat all uIDs in data
     concat_dict = {f'{uid}_{k}': v for k, v in data.items()}
     global_image_dict.update(concat_dict)
 
 
 def create_candidate_dict():
-    fp_folder_filepath = '/Users/noaschwartz/Downloads/finalproject_065/fingerprints'  # same as root
+    '''
+    creates a global dictionary with shortened filenames and uIDs by walking through
+    all json dicts in fp_folder_filepath.
+
+    :return: None
+    '''
+    fp_folder_filepath = './fingerprints'  # same as root
 
     # list of json file names
     img_file_names = [img_json for img_json in os.listdir(fp_folder_filepath) if img_json.endswith('.json')]
 
     # walk through the directory
     for filename in img_file_names:
-
         short_name = shorten_img_name(filename)
-
         if short_name != filename:
             # rename img file name with new short uid (ignore file ext)
             filename_no_extension, extension = os.path.splitext(filename)
@@ -76,15 +97,30 @@ def create_candidate_dict():
             # print("file: " + path1 + "  renamed to: " + path2)
 
         uid = os.path.splitext(short_name)[0]  # removes .json
-        # print(uid)
         concat_uid_with_tile_label(fp_folder_filepath,short_name,uid)
 
 
 def distance_score_between_fingerprint(query_fingerpint, candidate_fingerprint):
+    '''
+    Calculates the euclidean distance between two fingerprints.
+
+    :param query_fingerpint: np array of the query fingerprint
+    :param candidate_fingerprint: np array of the candidate fingerprint
+    :return: euclidean distance between the two fingerprints
+    '''
     dist = np.linalg.norm(query_fingerpint - candidate_fingerprint)
     return dist
 
 def nearest_k_tiles(query_tile_uid, candidate_dict, k):
+    '''
+    Calculates the k most similar fingerprints to a query fingerprints.
+
+    :param query_tile_uid: the uID of the query, string
+    :param candidate_dict: the dictionary mapping {uID : fingerprint} for all images tiles in dataset
+    :param k: int, number of closest fingerprints to return
+    :return: keys of the k nearest tiles to the query
+    '''
+    #get the query fingerprint from candidate dict
     query_fingerprint = candidate_dict[query_tile_uid]
     cand_dist_dict = {}  # maps uid of tile to fingerprint distance
 
@@ -98,6 +134,13 @@ def nearest_k_tiles(query_tile_uid, candidate_dict, k):
     return smallest_k_cand.keys()
 
 def matplot_images(images, title = False):
+    '''
+    Plot a query result using matplotlib
+
+    :param images: array of images to plot
+    :param title: Bool, controls if plot has a title or not
+    :return: f, axarr, matplotlib objects specifying the plot
+    '''
     nrows, ncols = 1 , len(images)
     f, axarr = plt.subplots(nrows=nrows, ncols=ncols)
 
@@ -118,6 +161,15 @@ def matplot_images(images, title = False):
     return f, axarr
 
 def manage_query(q = None, K = 11):
+    '''
+    Queries the fingerprints. Starts with a query string q specifying which image tile to find matches for,
+    then computes euclidean distance between this tile and all other tiles in the dataset,
+    returning the k most similar images of those. Then results are plotted with matplotlib.
+
+    :param q: query string, uID of image tile
+    :param K: the value of K for the K most similar tiles
+    :return: f, axarr, matplotlib objects plotting the result
+    '''
 
     #load in global dict
     print('Loading global dict...')
@@ -166,19 +218,16 @@ def manage_query(q = None, K = 11):
     return f, axarr
 
 
+if __name__ == '__main__':
 
+    #query the dataset with a random query, or choose a value to pass into manage_query from the list below!
+    manage_query()
 
-manage_query()
-
-
-#print(nearest_k_tiles('1d4fbe33f3_25', global_image_dict, 6))
-
-
-good_query = ['ec09336a6f_250', #trees / barren land
-              '1476907971_558', #more barren land (no trees)
-              '15efe45820_3757', #petit houses w cars
-              '107f24d6e9_2798', #tree shadows on short grass
-              '39e77bedd0_480', #solar panels
-              'f9f43e5144_514', #riverside in winter w trees,
-              '107f24d6e9_2578', #nice houses w pool
-              ]
+    good_query = ['ec09336a6f_250', #trees / barren land
+                '1476907971_558', #more barren land (no trees)
+                '15efe45820_3757', #petit houses w cars
+                '107f24d6e9_2798', #tree shadows on short grass
+                '39e77bedd0_480', #solar panels
+                'f9f43e5144_514', #riverside in winter w trees,
+                '107f24d6e9_2578', #nice houses w pool
+                ]
